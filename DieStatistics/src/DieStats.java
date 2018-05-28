043c3c2e230e6b25f85ecf484d6f43b5bb7a8093
Cory <Falcongray17@gmail.com>
2018-05-28 14:09:19 -0400

    j

--- DieStatistics/src/DieStats.java
+++ DieStatistics/src/DieStats.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.*;
 
 /** DieStats -
  * @author cory
@@ -10,12 +11,15 @@ public class DieStats {
 
 	static Scanner keyboard = new Scanner(System.in);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		int choice, numDie, numSides;
 		long numRolls;
 		boolean continueRun;
 		String continueRunning = "";
 		String[] options = {"Die - Empirical", "Die - Empirical - Combination", "Die - Classical", "Die - Classical - Combination"};
+		FileWriter fwriter = new FileWriter("log.txt", true);
+		PrintWriter logOut = new PrintWriter(fwriter);
+		
 		//handles calling of functions - menu
 		do {
 			System.out.println("\t-= Die Stats =-\n\nThis program allows the user to enter a number\nof dice, number of sides, and get the empirical\nand classical probabilities of both.");
@@ -32,20 +36,24 @@ public class DieStats {
 				numSides = keyboard.nextInt(); keyboard.nextLine();
 				System.out.println("Please enter number of rolls: ");
 				numRolls = keyboard.nextLong(); keyboard.nextLine();
-				dieEmpirical(numSides, numDie, numRolls);
+				logOut.printf("Empirical - Rolls: %d\tDie: %d\tSides: %d", numRolls, numDie, numSides);
+				dieEmpirical(numSides, numDie, numRolls, logOut);
 				break;
 			case 1: //empirical combo
 				System.out.println("Please enter number of rolls: ");
 				numRolls = keyboard.nextLong(); keyboard.nextLine();
-				dieEmpiricalCombo(numDie, numRolls);
+				logOut.printf("Empirical Combo - Rolls: %d\tDie: %d", numRolls, numDie);
+				dieEmpiricalCombo(numDie, numRolls, logOut);
 				break;
 			case 2: //classical
 				System.out.println("Please enter number of sides on those die: ");
 				numSides = keyboard.nextInt(); keyboard.nextLine();
-				dieClassical(numSides, numDie);
+				logOut.printf("Classical - Die: %d\tSides: %d", numDie, numSides);
+				dieClassical(numSides, numDie, logOut);
 				break;
 			case 3: //classical combo
-				dieClassicalCombo(numDie);
+				logOut.printf("Classical Combo - Die: %d\t", numDie);
+				dieClassicalCombo(numDie, logOut);
 				break;
 			}
 			System.out.println("Would you like to run again?");
@@ -55,7 +63,7 @@ public class DieStats {
 			else
 				continueRun = true;
 		} while(continueRun);
-		
+		logOut.close();
 		keyboard.close();
 	}
 	
@@ -65,7 +73,7 @@ public class DieStats {
 	 * @param numDie
 	 * @param numRolls
 	 */
-	public static void dieEmpirical(int numSides, int numDie, long numRolls) {
+	public static void dieEmpirical(int numSides, int numDie, long numRolls, PrintWriter logOut) {
 		int[] tally = new int[((numSides * numDie)-(numDie - 1))];
 		double xBar = 0;
 		int roll = 0;
@@ -80,16 +88,18 @@ public class DieStats {
 			tally[roll-numDie]++;
 			roll = 0;
 		}
+		logOut.println("");
 		for(int i = 0; i < tally.length; i++) {
 			System.out.printf("Roll of %d: %d rolls\n", numDie + i , tally[i]);
+			logOut.printf("%d,", tally[i]);
 			xBar += (tally[i]*(numDie+i));
 		}
+		logOut.println("");
 		System.out.printf("Total rolls: %d\n", numRolls);
 		for(int i = 0; i < tally.length; i++)
 			System.out.printf("Percentage of total for roll of %d: %.4f%s\n", numDie + i , ((double)tally[i]/(double)numRolls)*100, "%");
 		System.out.printf("Average roll: %.4f\n", xBar/numRolls);
 		
-		//option to pop result into file for further processing
 	}
 	
 	/** dieEmpiricalCombo - 
@@ -97,14 +107,16 @@ public class DieStats {
 	 * @param numDie
 	 * @param numRolls
 	 */
-	public static void dieEmpiricalCombo(int numDie, long numRolls) {
+	public static void dieEmpiricalCombo(int numDie, long numRolls, PrintWriter logOut) {
 		int totalSides = 0, numSides;
 		double xBar = 0;
 		int roll = 0;
 		Die[] dice = new Die[numDie]; //easy to add new dice of new sides and roll them
+		logOut.print("Sides: ");
 		for(int i = 0; i < numDie; i++) {
 			System.out.printf("How many sides for die #%d: ", i+1);
 			numSides = keyboard.nextInt(); keyboard.nextLine();
+			logOut.print(numSides + " ");
 			dice[i] = new Die(numSides);
 			totalSides += numSides;
 		}
@@ -121,16 +133,18 @@ public class DieStats {
 				System.out.print(". ");
 			roll = 0;
 		}
+		logOut.println("");
 		for(int i = 0; i < tally.length; i++) {
 			System.out.printf("Roll of %d: %d rolls\n", numDie + i , tally[i]);
+			logOut.printf("%d,", tally[i]);
 			xBar += (tally[i]*(numDie+i));
 		}
+		logOut.println("");
 		System.out.printf("Total rolls: %d\n", numRolls);
 		for(int i = 0; i < tally.length; i++)
 			System.out.printf("Percentage of total for roll of %d: %.4f%s\n", numDie + i , ((double)tally[i]/(double)numRolls)*100, "%");
 		System.out.printf("Average roll: %.4f\n", xBar/numRolls);
 		
-		//option to pop result into file for further processing
 	}
 	
 	/** dieClassical -
@@ -138,7 +152,7 @@ public class DieStats {
 	 * @param numSides
 	 * @param numDie
 	 */
-	public static void dieClassical(int numSides, int numDie) {
+	public static void dieClassical(int numSides, int numDie, PrintWriter logOut) {
 		int[] results = new int[] {1}, die = new int[numDie];
 		double xBar = 0;
 		int totalOutcomes = 1, resultsLength = 1;
@@ -155,29 +169,33 @@ public class DieStats {
 			resultsLength = 1;
 		}
 		//get prob from all the results
+		logOut.println("");
 		for(int i = 0; i < results.length; i++) {
 			System.out.printf("Classical percentage of total for roll of %d: %.4f%s\n", numDie + i , ((double)results[i]/(double)totalOutcomes*100), "%");
+			logOut.printf("%d,", results[i]);
 			xBar += (results[i]*(numDie+i));
 			//do 68%/95%/99% values here too
 			//grab middle, keep adding sides until it is greater then 65%, then 95%. output ranges that we hit at max, and figure out the mirror
 		}
+		logOut.println("");
 		System.out.printf("Average roll: %.4f\n", xBar/totalOutcomes);
 		
-		//option to pop result into file for further processing
 	}
 
 	/** dieClassicalCombo -
 	 * 
 	 * @param numDie
 	 */
-	public static void dieClassicalCombo(int numDie) {
+	public static void dieClassicalCombo(int numDie, PrintWriter logOut) {
 		int[] results = new int[] {1}, die = new int[numDie];
 		double xBar = 0;
 		int totalOutcomes = 1, resultsLength = 1;
 		//get sides of die
+		logOut.print("Sides: ");
 		for(int i = 0; i < numDie; i++) {
 			System.out.printf("Please enter sides for die #%d: ", i+1);
 			die[i] = keyboard.nextInt(); keyboard.nextLine();
+			logOut.print(die[i] + " ");
 			totalOutcomes *= die[i]; 
 		}
 		for(int i = 0; i < die.length; i++) {
@@ -188,11 +206,13 @@ public class DieStats {
 			resultsLength = 1;
 		}
 		//get prob from all the results
+		logOut.println("");
 		for(int i = 0; i < results.length; i++) {
 			System.out.printf("Classical percentage of total for roll of %d: %.4f%s\n", numDie + i , ((double)results[i]/(double)totalOutcomes*100), "%");
+			logOut.printf("%d,", results[i]);
 			xBar += (results[i]*(numDie+i));
 		}
-		//option to pop result into file for further processing
+		logOut.println("");
 		System.out.printf("Average roll: %.4f\n", xBar/totalOutcomes);
 	}
 	
